41e82111663a5aea1e9251fe76c5cd12e59cace2
andrew-j-millward <andrew.j.millward@gmail.com>
2022-07-19 00:01:18 -0400

    Revised header with animation styling

--- src/components/LinkSpan.js
+++ src/components/LinkSpan.js
@@ -1,6 +1,6 @@
 const LinkSpan = ({ page, address, active }) => {
   return (
-    <div className="App-header-selected">
+    <div>
       <a href={address}>
         <h1 className="App-link App-header-selected">{page}</h1>
       </a>
