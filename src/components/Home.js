1734f9eeea2a6b9d89d7e21742d9b80206a4e910
andrew-j-millward <andrew.j.millward@gmail.com>
2022-07-20 23:55:44 -0400

    Animated typing homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,30 @@
+import { GrPersonalComputer } from "react-icons/gr";
+import { SiGitbook } from "react-icons/si";
+
 const Home = ({ profile }) => {
   return (
-    <div className="App-profile Show-box">
+    <div className="App-profile">
       <img src={profile} className="App-profile-image" alt="Profile Headshot" />
       <div className="App-profile-text">
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
-        <p>Test</p>
+        <div>
+          <div className="typer">
+            <h1>Hi, my name is Andrew!</h1>
+            <h2>I am a software engineer.</h2>
+            <br></br>
+          </div>
+          <div className="typer-1">
+            <p>
+              <GrPersonalComputer className="align-icon" />
+              TDP Software Engineer Associate at Capital One
+            </p>
+          </div>
+          <div className="typer-2">
+            <p>
+              <SiGitbook color="black" className="align-icon" />
+              M.S. in Computer Science and Engineering
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
