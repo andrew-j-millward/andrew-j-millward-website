0cd3eecfd15664c4c28e1aae13fc375a2d3e31e8
andrew-j-millward <andrew.j.millward@gmail.com>
2022-07-25 22:00:09 -0400

    Added SVG files for languages, frameworks, and tools

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,12 @@ import "./About.scss";
 const About = () => {
   return (
     <div className="App-about-text">
-      <p>Test</p>
+      <p>
+        Hello, my name is Andrew Millward, and I am a software engineer
+        currently based in Richmond, Virginia. I enjoy working on a wide range
+        of projects using different frameworks and languages and am eager to
+        learn more.
+      </p>
     </div>
   );
 };
