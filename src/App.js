9bc13b83661d15dbd92ef7b0c0676eae2b1290ca
andrew-j-millward <andrew.j.millward@gmail.com>
2022-07-26 01:46:17 -0400

    Init portfolio commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
 import profile from "./andrew-millward-professional-2-removebg-hexagon-right.png";
 
@@ -25,7 +26,7 @@ function App() {
             }
           />
           <Route exact path="/about" element={<About />} />
-          <Route exact path="/portfolio" element={<h1>Hi3</h1>} />
+          <Route exact path="/portfolio" element={<Portfolio />} />
           <Route exact path="/contact" element={<h1>Hi4</h1>} />
         </Routes>
         <Footer />
---
+++ src/components/Portfolio.js
@@ -0,0 +1,11 @@
+import "./Portfolio.scss";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1 className="portfolio-label">My Portfolio</h1>
+    </div>
+  );
+};
+
+export default Portfolio;
