499985ee93d30c9c699787d7ccac0045463b61d0
andrew-j-millward <andrew.j.millward@gmail.com>
2022-08-03 01:13:43 -0400

    Some resizing code for varying resolutions both in CSS and React effects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,27 +1,72 @@
+import { useState, useEffect } from "react";
 import "./Portfolio.scss";
 import TimelineCard from "./TimelineCard";
 import proj1 from "./assets/projects/L2-UniFrac-Cover-Photo.png";
 import center from "./assets/languages/Python-logo-notext.svg";
 
+const getWindowSize = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height };
+};
+
+const toggleRight = (windowSize) => {
+  return windowSize.width <= 1024 ? true : false;
+};
+
 const Portfolio = () => {
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
     <div>
       <h1 className="portfolio-label">My Portfolio</h1>
       <div>
         <div className="timeline">
-          <TimelineCard leftBool={true} visible={true} image={proj1} />
+          {!toggleRight(windowSize) ? (
+            <TimelineCard
+              leftBool={!toggleRight(windowSize)}
+              visible={!toggleRight(windowSize)}
+              image={!toggleRight(windowSize) ? proj1 : null}
+            />
+          ) : (
+            ""
+          )}
           <div className="timeline-strip">
             <div class="line"></div>
           </div>
-          <TimelineCard leftBool={false} visible={false} />
+          <TimelineCard
+            leftBool={toggleRight(windowSize)}
+            visible={toggleRight(windowSize)}
+            image={toggleRight(windowSize) ? proj1 : null}
+          />
         </div>
         <div className="gap-content">
           <div className="timeline-strip">
-            <div class="line"></div>
+            <div className="line"></div>
           </div>
+          {toggleRight(windowSize) ? <div className="buffer"></div> : ""}
         </div>
         <div className="timeline">
-          <TimelineCard leftBool={true} visible={false} />
+          {!toggleRight(windowSize) ? (
+            <TimelineCard
+              leftBool={toggleRight(windowSize)}
+              visible={toggleRight(windowSize)}
+              image={toggleRight(windowSize) ? proj1 : null}
+            />
+          ) : (
+            ""
+          )}
           <div className="timeline-strip">
             <div class="line"></div>
           </div>
